62e391d34c29bf0a02f268e2a60141a8cee9413e
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2024-06-23 19:12:12 -0700

    Agregar InicioSesion y Registro

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AcercaDe from './components/AcercaDe';
 import { MenuLateral } from "./components/MenuLateral";
 import GruposDeTrabajo from "./components/GruposDeTrabajo";
 import { VistaGeneral } from "./components/VistaGeneral";
 import { GraficaIndividual } from "./components/GraficaIndividual";
+import Login from "./components/InicioSesion";
+import Registro from './components/Registro';
 
 function App() {
+  const [isSesionIniciada, setSesionIniciada] = useState(false);
+  const [mostrarRegistro, setMostrarRegistro] = useState(false);
   const [contenido, setContenido] = useState('inicio');
   const [valor, setValor] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setSesionIniciada(true);
+    }
+  }, []);
 
   const cambiarContenido = (nuevoContenido, valor) => {
       setContenido(nuevoContenido);
       setValor(valor);
+    };
+
+    const handleLogin = () => {
+      setSesionIniciada(true);
+      localStorage.setItem('token', 'yourAuthTokenHere');
+    };
+  
+    const handleLogout = () => {
+      setSesionIniciada(false);
+      localStorage.removeItem('token');
+    };
+
+    const handleRegisterClick = () => {
+      setMostrarRegistro(true);
+    };
+
+    const handleBackToLogin = () => {
+      setMostrarRegistro(false);
+    };  
+
+    const renderizarContenidoPrincipal = () => {
+      if (!isSesionIniciada) {
+        return (
+          <div style={{ background: '#f0f0f0', minHeight: '100vh' }}>
+          {mostrarRegistro ? <Registro onBackToLogin={handleBackToLogin} /> : <Login onLogin={handleLogin} onRegisterClick={handleRegisterClick} />}
+        </div>
+          );
+      }
+  
+      return (
+        <div>
+          <MenuLateral cambiarContenido={cambiarContenido} onLogout={handleLogout} />
+          <div id="contenido">
+            {renderizarContenido()}
+          </div>
+        </div>
+      );
     };
 
   const renderizarContenido = () => {
@@ -39,15 +86,11 @@ function App() {
       }
     };
 
-  return (
-      <body>
-          <MenuLateral cambiarContenido={cambiarContenido} />
-          <div id="contenido">
-              {renderizarContenido()}
-          </div>
-
-      </body>
-  );
-}
+    return (
+      <div className="App">
+        {renderizarContenidoPrincipal()}
+      </div>
+    );
+  }
 
 export default App;
